48833c5e2b1e5ec09b97ff54502fadfc10f6e697
Daniel Morante <daniel@morante.net>
2014-07-11 20:36:10 -0400

    Fix build error

--- Dockerfile
+++ Dockerfile
@@ -26,9 +26,6 @@ ADD ./multifaucet/ /var/www/html/
 ADD ./do-config.sh /tmp/do-config.sh
 RUN /bin/sh /tmp/do-config.sh
 
-# Make configuration and cold wallet directory writable so the user can continue withe the web installer
-RUN chown -R apache:apache /var/www/html/config/ && chmod -R 700 /var/www/html/config/ && chcon -Rt httpd_sys_content_rw_t /var/www/html/config/ && chown -R apache:apache /var/db/multifaucet/ && chmod -R 700 /var/db/multifaucet/ && chcon -Rt httpd_sys_content_rw_t /var/db/multifaucet/
-
 #Open port 80 on Firewall
 RUN firewall-cmd --permanent --add-port=80/tcp
 
--- do-config.sh
+++ do-config.sh
@@ -31,3 +31,13 @@ echo "define(\"PAYMENT_GW_RPC_PASS\", \"${MULTIFAUCET_WALLET_PASS}\");" >> /var/
 echo "define(\"PAYMENT_GW_DATAFILE\", \"/var/db/multifaucet/balance.dat\");" >> /var/www/html/config/wallet.conf.php
 echo "define(\"ADDRESS_VERSION\", \"0\");" >> /var/www/html/config/wallet.conf.php
 echo "?>" >> /var/www/html/config/wallet.conf.php
+
+# Make configuration directory writable so the user can continue withe the web installer
+chown -R apache:apache /var/www/html/config/
+chmod -R 700 /var/www/html/config/ 
+chcon -Rt httpd_sys_content_rw_t /var/www/html/config/ 
+
+# Make cold wallet directory writable so the user can continue withe the web installer
+chown -R apache:apache /var/db/multifaucet/ 
+chmod -R 700 /var/db/multifaucet/ 
+chcon -Rt httpd_sys_content_rw_t /var/db/multifaucet/
