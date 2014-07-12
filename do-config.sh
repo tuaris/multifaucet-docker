de1eb4eb3b8984e01948f92d45c754ae6cb6b2c0
Daniel Morante <daniel@morante.net>
2014-07-11 20:48:14 -0400

    Comment out context changes

--- do-config.sh
+++ do-config.sh
@@ -3,6 +3,8 @@
 # Run MySQL/MariaDB
 systemctl start mariadb.service
 
+sleep 5
+
 # Genorate random password
 MULTIFAUCET_DB_PASS=`date | md5sum | head -c 8`
 
@@ -35,9 +37,9 @@ echo "?>" >> /var/www/html/config/wallet.conf.php
 # Make configuration directory writable so the user can continue withe the web installer
 chown -R apache:apache /var/www/html/config/
 chmod -R 700 /var/www/html/config/ 
-chcon -Rt httpd_sys_content_rw_t /var/www/html/config/ 
+#chcon -Rt httpd_sys_content_rw_t /var/www/html/config/ 
 
 # Make cold wallet directory writable so the user can continue withe the web installer
 chown -R apache:apache /var/db/multifaucet/ 
 chmod -R 700 /var/db/multifaucet/ 
-chcon -Rt httpd_sys_content_rw_t /var/db/multifaucet/
+#chcon -Rt httpd_sys_content_rw_t /var/db/multifaucet/
